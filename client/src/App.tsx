68a6805164779cd00e6bbbd4bde182d77d10a57c
cyj <cyd5538@naver.com>
2023-12-12 17:14:33 +0900

    Feat : react-router-dom setting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
     </>
   )
 }

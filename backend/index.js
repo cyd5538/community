2c3b6be867262014642b6c02f40dfe3d40078753
cyj <cyd5538@naver.com>
2023-12-13 19:42:30 +0900

    Fea: 로그인 회원가입, 유저정보 API

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 5000;
 const app = express();
 connectDB()
 
+app.use('/api/users', require('./routes/userRoutes.js'))
+
 app.listen(port, () => {
   console.log(`server connected ${port}`)
 })

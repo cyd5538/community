e2c5058e7b0067bf46b0b2fc848a3f9e2a4c3c7e
cyj <cyd5538@naver.com>
2023-12-14 08:25:41 +0900

    Feat: cors 미들웨어 추가

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 5000;
 const app = express();
 connectDB()
 
+app.use(express.json());
+app.use(cors());
 app.use('/api/users', require('./routes/userRoutes.js'))
 
 app.listen(port, () => {

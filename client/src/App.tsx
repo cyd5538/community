8e9746ae330f7a4bc4d04eda0a85cdddbb43fa93
cyj <cyd5538@naver.com>
2023-12-14 10:26:37 +0900

    Feat: mypage 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
-import Login from "./pages/login"
-import Register from "./pages/register"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Myprofile from "./pages/Myprofile"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/login" element={<Login />}/>
         <Route path="/Register" element={<Register />}/>
+        <Route path="/me" element={<Myprofile />}/>
       </Routes>
     </>
   )

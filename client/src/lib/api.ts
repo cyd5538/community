7646eb0189fdf6d30fd493c3983d0da15886cb65
cyj <cyd5538@naver.com>
2023-12-14 10:23:19 +0900

    Feat: 로그인 구현

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -12,3 +12,13 @@ export const register = async (userData) => {
   return response.data
 }
 
+export const login = async (userData) => {
+  const response = await axios.post(API_URL + 'login', userData)
+
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data))
+  }
+
+  return response.data
+}
+
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,21 +1,72 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { useState } from 'react';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useNavigate } from 'react-router-dom';
+import { login } from '../lib/api';
+import axios from 'axios';
+
+interface LoginForm {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
   const navigate = useNavigate();
+  const [formData, setFormData] = useState<LoginForm>({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await login(formData);
+      alert("로그인 완료");
+      navigate('/');
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        alert(error.response?.data.error);
+      }
+      else alert('로그인 중에 에러가 발생했습니다.');
+    }
+  };
 
   return (
-    <div className="w-full h-screen sm:bg-green-500 bg-white  flex justify-center items-center">
-      <form className="w-full sm:w-96 px-4 py-4 flex flex-col gap-8 drop-shadow-md bg-white rounded-xl">
+    <div className="w-full h-screen sm:bg-green-500 bg-white flex justify-center items-center">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full sm:w-96 px-4 py-4 flex flex-col gap-8 drop-shadow-md bg-white rounded-xl"
+      >
         <h1 className="font-bold text-center text-2xl">Login</h1>
-        <Input type="email" placeholder="Email을 입력해주세요." />
-        <Input type="password" placeholder="비밀번호를 입력해주세요." />
-        <Button>Login</Button>
+        <Input
+          type="email"
+          name="email"
+          placeholder="Email을 입력해주세요."
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="비밀번호를 입력해주세요."
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <Button type="submit">Login</Button>
         <p className="text-sm text-center">
           비회원이신가요?
-          <span 
-            onClick={() => { navigate('/register'); }}
+          <span
+            onClick={() => {
+              navigate('/register');
+            }}
             className="underline pl-2 cursor-pointer"
           >
             회원가입
@@ -23,7 +74,7 @@ const Login = () => {
         </p>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;

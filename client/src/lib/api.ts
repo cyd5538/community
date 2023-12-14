a907339be5580d9cd0d4649b98762d9cb6612abd
cyj <cyd5538@naver.com>
2023-12-14 14:18:54 +0900

    Feat: myinfo data 가져오기 구현

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -6,7 +6,7 @@ export const register = async (userData) => {
   const response = await axios.post(API_URL, userData)
 
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
+    localStorage.setItem('token', JSON.stringify(response.data.token))
   }
 
   return response.data
@@ -16,9 +16,20 @@ export const login = async (userData) => {
   const response = await axios.post(API_URL + 'login', userData)
 
   if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
+    localStorage.setItem('token', JSON.stringify(response.data.token))
   }
 
   return response.data
 }
+
+export const getMyInfo = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+  const response = await axios.post(API_URL + 'me',{}, config);
+
+  return response.data;
+};
 
--- client/src/pages/Myprofile.tsx
+++ client/src/pages/Myprofile.tsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { getMyInfo } from '../lib/api';
+import axios from 'axios';
 
 const Myprofile = () => {
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    const fetchMyInfo = async () => {
+      try {
+        if (token) {
+          const response = await getMyInfo(token);
+          setUserInfo(response);
+        }
+      } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+          alert(error.response?.data.error);
+        }
+      }
+    };
+
+    fetchMyInfo();
+  }, []); 
+
   return (
     <div>
       

e97b1915462160cb2d7116a8f227d53ca6092a81
cyj <cyd5538@naver.com>
2023-12-16 21:09:33 +0900

    Design: Containerdesign

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,21 @@ import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Myprofile from "./pages/Myprofile"
 import Sidebar from "./components/sidebar/sidebar"
+import Container from "./components/ui/Container"
 
 function App() {
 
   return (
     <div className="flex">
       <Sidebar />
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/Register" element={<Register />}/>
-        <Route path="/me" element={<Myprofile />}/>
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/Register" element={<Register />}/>
+          <Route path="/me" element={<Myprofile />}/>
+        </Routes>
+      </Container>
     </div>
   )
 }
---
+++ client/src/components/ui/Container.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from 'react';
+import useSidebar from '@/hook/useSidebarWidth';
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  const sidebarWidth = useSidebar();
+
+  return (
+    <div className={`w-full p-6 ${sidebarWidth.Open ? 'ml-16' : 'ml-72'}`}>
+      {children}
+    </div>
+  );
+}
+
+export default Container;

0ced2b11891173002e11f14c7348607557c3d1b6
cyj <cyd5538@naver.com>
2023-12-16 14:29:15 +0900

    Fix: like, comment model require

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,9 @@ app.use(cors());
 app.use('/api/users', require('./routes/userRoutes.js'))
 app.use('/api/posts', require('./routes/postRoutes.js'));
 
+require("./models/likeModel.js");
+require("./models/commentModel.js");
+
 app.listen(port, () => {
   console.log(`server connected ${port}`)
 })

696b74a1467338ff437df33c426d0ab43be384ec
cyj <cyd5538@naver.com>
2023-12-16 13:56:14 +0900

    Feat: post react-query로 변경 file type 변경

--- client/src/components/Modal/PostModal.tsx
+++ client/src/components/Modal/PostModal.tsx
@@ -2,19 +2,55 @@ import { FormEvent, useState, useRef } from 'react';
 import { Input } from '@/components/ui/input';
 import { Textarea } from "@/components/ui/textarea";
 import { handlePostSubmit } from '@/lib/postApi';
-import Loading from '../ui/Loading';
-import Modal from '../ui/Modal';
+import Loading from '@/components/ui/Loading';
+import Modal from '@/components/ui/Modal';
 import usePostModel from '@/hook/userPostModel';
 import { IoMdClose } from "react-icons/io";
 import { CiImageOff } from "react-icons/ci";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const PostModal = () => {
   const [title, setTitle] = useState<string>('');
   const [description, setDescription] = useState<string>('');
-  const [file, setFile] = useState<string | null>(null);
+  const [file, setFile] = useState<File | null>(null);
   const [filePreview, setFilePreview] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const postModel = usePostModel();
+
+  const queryClient = useQueryClient()
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    const token = localStorage.getItem('token');
+
+    if(!title || !description) {
+      return alert("제목이나 내용을 입력해주세요")
+    }
+
+    try {
+      setLoading(true); 
+      await handlePostSubmit(title, description, file, token);
+      setTitle("")
+      setDescription("")
+      setFile(null)
+      alert("포스트가 완료되었습니다.")
+      postModel.onClose();
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false); 
+    }
+  };
+  
+  const updatePostMutation = useMutation({
+    mutationFn: handleSubmit,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["post"]});
+    },
+    onError : (err) => {
+      console.log(err)
+    }
+  })
 
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
@@ -34,34 +70,17 @@ const PostModal = () => {
         setFilePreview(event.target?.result as string);
       };
       reader.readAsDataURL(selectedFile);
+      setFile(selectedFile);
     } else {
       setFilePreview(null);
     }
-  };
-
-
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    const token = localStorage.getItem('token');
-
-    try {
-      setLoading(true); 
-      await handlePostSubmit(title, description, file, token);
-      setTitle("")
-      setDescription("")
-      setFile(null)
-      postModel.onClose()
-      alert("포스트가 완료되었습니다.")
-    } catch (error) {
-      console.log(error)
-    } finally {
-      setLoading(false); 
-    }
+    
   };
 
   if(loading) {
     return <Loading />
   }
+
   const bodyContent = (
     <form className='flex gap-4 flex-col'>
       <Input
@@ -86,7 +105,10 @@ const PostModal = () => {
         {filePreview ? 
         <div className='relative w-full'>
           <img className='w-full h-48 object-cover' src={filePreview} alt="File Preview" /> 
-          <div className='text-white absolute top-2 right-4 p-1 rounded-full bg-blue-300 cursor-pointer hover:bg-blue-500' onClick={() => setFilePreview(null)}>
+          <div 
+            className='text-white absolute top-2 right-4 p-1 rounded-full bg-blue-300 cursor-pointer hover:bg-blue-500' 
+            onClick={() => setFilePreview(null)
+          }>
             <IoMdClose className="text-xl"/>
           </div>
         </div> :
@@ -95,7 +117,10 @@ const PostModal = () => {
         </>
         }
         <div className='flex justify-end items-center'>
-          <button className='shadow-lg bg-green-400 text-white hover:bg-green-500 w-20 h-12 rounded-full' onClick={handleSubmit}>
+          <button 
+            className='shadow-lg bg-green-400 text-white hover:bg-green-500 w-20 h-12 rounded-full' 
+            onClick={(e) => updatePostMutation.mutate(e)}
+          >
             Post
           </button>
         </div>

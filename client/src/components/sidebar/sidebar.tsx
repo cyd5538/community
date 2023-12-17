fd76f5de3b899edc1c9f5cb0b9f0eb23df22b0c8
cyj <cyd5538@naver.com>
2023-12-17 22:30:11 +0900

    Feat: sidear 전역관리 삭제하고 sidebar에서 관리

--- client/src/components/sidebar/sidebar.tsx
+++ client/src/components/sidebar/sidebar.tsx
@@ -1,4 +1,4 @@
-import React, {} from "react";
+import React, { useState } from "react";
 import { CiMenuBurger } from "react-icons/ci";
 import { CiHome } from "react-icons/ci";
 import { AiTwotoneProfile } from "react-icons/ai";
@@ -6,7 +6,6 @@ import { IoSettings } from "react-icons/io5";
 import { FiMessageSquare } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import { IoFootballOutline } from "react-icons/io5";
-import useSidebar from "@/hook/useSidebarWidth";
 
 const Sidebar = () => {
   const menus = [
@@ -16,21 +15,20 @@ const Sidebar = () => {
     { name: "Profile", link: "/", icon: AiTwotoneProfile, margin: true },
     { name: "Setting", link: "/", icon: IoSettings },
   ];
-
-  const sidebarWidth = useSidebar()
-
+  const [open, setOpen] = useState(false);
+  
   return (
-    <section className="flex gap-6 fixed top-0 left-0 z-50">
+    <section className="flex fixed top-0 left-0 z-40 gap-6">
       <div
         className={`bg-green-700 min-h-screen ${
-          !sidebarWidth.Open ? "w-72" : "w-16"
+          open ? "w-40" : "w-16"
         } duration-500 text-gray-100 px-4`}
       >
         <div className="py-3 flex justify-end">
           <CiMenuBurger
             size={26}
             className="cursor-pointer"
-            onClick={sidebarWidth.Open ? sidebarWidth.onClose : sidebarWidth.onOpen}
+            onClick={() => setOpen(!open)}
           />
         </div>
         <div className="mt-4 flex flex-col gap-4 relative">
@@ -48,14 +46,14 @@ const Sidebar = () => {
                   transitionDelay: `${i}00ms`,
                 }}
                 className={`whitespace-pre duration-500 ${
-                  sidebarWidth.Open && "opacity-0 translate-x-4 overflow-hidden"
+                  !open && "opacity-0 translate-x-4 overflow-hidden"
                 }`}
               >
                 {menu?.name}
               </h2>
               <h2
                 className={`${
-                  !sidebarWidth.Open && "hidden"
+                  open && "hidden"
                 } absolute left-48 bg-green-300 font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
               >
                 {menu?.name}
--- client/src/components/ui/Container.tsx
+++ client/src/components/ui/Container.tsx
@@ -1,15 +1,13 @@
 import React, { ReactNode } from 'react';
-import useSidebar from '@/hook/useSidebarWidth';
 
 interface ContainerProps {
   children: ReactNode;
 }
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
-  const sidebarWidth = useSidebar();
 
   return (
-    <div className={`w-full p-6 ${sidebarWidth.Open ? 'ml-16' : ''}`}>
+    <div className={`w-full p-6 ml-16`}>
       {children}
     </div>
   );

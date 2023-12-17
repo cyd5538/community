39d6aea8b12d2418cb3d1e2fbe069e07717424f4
cyj <cyd5538@naver.com>
2023-12-17 22:05:01 +0900

    Design: Container sidebar width 수정

--- client/src/components/ui/Container.tsx
+++ client/src/components/ui/Container.tsx
@@ -9,7 +9,7 @@ const Container: React.FC<ContainerProps> = ({ children }) => {
   const sidebarWidth = useSidebar();
 
   return (
-    <div className={`w-full p-6 ${sidebarWidth.Open ? 'ml-16' : 'ml-72'}`}>
+    <div className={`w-full p-6 ${sidebarWidth.Open ? 'ml-16' : ''}`}>
       {children}
     </div>
   );

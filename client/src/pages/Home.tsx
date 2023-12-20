3c99448620d218db1361da7898043a546954efbe
cyj <cyd5538@naver.com>
2023-12-20 15:25:52 +0900

    Design: home design 수정

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -9,10 +9,10 @@ const Home = () => {
       <Searchbar />
       <PostModal />
       <div className='pt-16 w-full flex'>
-        <div className='w-full sm:w-2/3 border-gray-10 border-[1px] p-4'>
+        <div className='w-full md:w-2/3 border-gray-10 border-[1px] p-4'>
           <Posts />
         </div>
-        <div className='hidden sm:block bg-gray-400'>
+        <div className='hidden md:block bg-gray-400'>
 
         </div>
       </div>

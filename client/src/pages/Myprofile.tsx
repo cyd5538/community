c922d0774f40362a8f39244f8dfe13e8ee139492
cyj <cyd5538@naver.com>
2023-12-26 18:35:25 +0900

    Feat: profile page tab 적용

--- client/src/pages/Myprofile.tsx
+++ client/src/pages/Myprofile.tsx
@@ -1,11 +1,22 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import useUserInfo from '@/hook/getUser';
 
 const Myprofile = () => {
-  const userInfo = useUserInfo();
 
   return (
     <div>
-      
+      <Tabs defaultValue="profile" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="profile">내 정보</TabsTrigger>
+          <TabsTrigger value="mypost">내 글 목록</TabsTrigger>
+        </TabsList>
+        <TabsContent value="profile">
+          내 정보
+        </TabsContent>
+        <TabsContent value="mypost">
+          내 글 목록
+        </TabsContent>
+      </Tabs>
     </div>
   )
 }

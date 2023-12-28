a560df02d8ee329e9aa0879b4a580f17ba14a5f1
cyj <cyd5538@naver.com>
2023-12-28 12:33:35 +0900

    Design: searchbar fixed

--- client/src/components/Home/Searchbar.tsx
+++ client/src/components/Home/Searchbar.tsx
@@ -3,12 +3,14 @@ import { Search } from 'lucide-react'
 const Searchbar = () => {
 
   return (
-    <form className='top-2 right-2 absolute'>
-      <input className='pl-10 border-green-600 border-[1px] focus:border-none py-2 rounded-full focus:bg-green-100' type="text" placeholder="" />
-      <span className='absolute top-[12px] left-4'>
-        <Search size={20}/>
-      </span>
-    </form>
+    <div className='fixed top-0 left-[-4px] w-full h-14 bg-gray-100 z-20'>
+      <form className='top-2 right-12 absolute'>
+        <input className='pl-10 border-green-600 border-[1px] focus:border-none py-2 rounded-full focus:bg-green-100' type="text" placeholder="" />
+        <span className='absolute top-[12px] left-4'>
+          <Search size={20}/>
+        </span>
+      </form>
+    </div>
   )
 }
 

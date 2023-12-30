433496b075ea50c8bad765443ce16f18c213f3d2
cyj <cyd5538@naver.com>
2023-12-30 10:49:17 +0900

    Design: tab width  변경

--- client/src/pages/Myprofile.tsx
+++ client/src/pages/Myprofile.tsx
@@ -14,7 +14,7 @@ const Myprofile = () => {
 
   return (
     <div>
-      <Tabs defaultValue="profile" className="w-[400px]">
+      <Tabs defaultValue="profile" className="w-full">
         <TabsList>
           <TabsTrigger value="profile">내 정보</TabsTrigger>
           <TabsTrigger value="mypost">내 글 목록</TabsTrigger>

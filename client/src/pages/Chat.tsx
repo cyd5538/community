e09b4f94aa42037fd35ad2bae61ed2a95ed89af2
cyj <cyd5538@naver.com>
2024-01-10 23:47:03 +0900

    Feat: socket 연결

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import socket from '../lib/socket';
+import useAuth from '@/store/useAuth';
+import { useQuery } from '@tanstack/react-query';
 
-const Chat = () => {
+const Chat = () => {  
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+  const [rooms, setRooms] = useState([])
+  
+  const { getMe } = useAuth();
+
+  const { data } = useQuery({
+    queryKey: ['users'],
+    queryFn: getMe,
+  });
+  
+  useEffect(() => {
+    if(data) {
+      socket.emit("login", data.nickname, (res) => {})
+    }
+  },[data])
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessageList((prevState) => prevState.concat(message))
+    })
+  },[])
+
+  const sendMessage = (e) => {
+    e.preventDefault()
+
+    socket.emit("sendMessage", message, (res) => {
+      
+    })
+  }
+  console.log(messageList)
   return (
-    <div>
-      Chat
-    </div>
+    <form onSubmit={sendMessage}>
+      <input placeholder='chat' type="text" value={message} onChange={(e) => setMessage(e.target.value)}/>
+    </form>
   )
 }
 

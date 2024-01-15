68aca6ad28d69088297302bec25dfaacd2e66e0b
cyj <cyd5538@naver.com>
2024-01-15 19:50:53 +0900

    Feat: socket.io chat 구현
    joinRoom 방입장
    leaveRoom 방퇴장
    sendMessage 메세지 전송
    disconnect 연결 끊기

--- backend/utils/chat.js
+++ backend/utils/chat.js
@@ -1,33 +1,78 @@
 const User = require("../models/userModel");
-const chatController = require("../controller/chatController");
+const Room = require("../models/roomModel");
+const Chat = require('../models/chatModel');
 
 module.exports = function(io) {
   // 말하는 함수 emit
   // 듣는 함수 on
-   io.on("connection", async(socket) => {
-    
-    // 사용자 들어옴 user socket.id에 
-    socket.on('login', async(userName, cb) => {
+  io.on('connection', (socket) => {
+  
+    // 유저 채팅방 입장
+    socket.on('joinRoom', async (roomId, userId) => {
+      socket.join(roomId);
+      console.log(roomId, userId)
+      // 사용자의 소켓 ID를 업데이트
+      const user = await User.findByIdAndUpdate(userId, { socketId: socket.id });
+
+      // 채팅방 정보
+      const room = await Room.findById(roomId).populate('owner').populate('members');
+
+      // 사용자에게 채팅방 정보 전송
+      socket.emit('currentRoomInfo', room);
+    });
+  
+    // 유저 채팅방 퇴장
+    socket.on('leaveRoom', (roomId) => {
+      socket.leave(roomId);
+    });
+
+    // 채팅 메시지 전송
+    socket.on('sendMessage', async (roomId, userId, message) => {
       try {
-        const user = await chatController.saveUser(userName, socket.id);
-        cb({ok: true, data: user})
+        const user = await User.findById(userId);
+    
+        if (!user) {
+          console.error('유저가 없습니다.');
+          return;
+        }
+        // 채팅 메시지 저장
+        const chat = new Chat({
+          chat: message,
+          user: {
+            id: user._id,
+            name: user.name,
+            profileImg: user.profileImage,
+          },
+          room: roomId,
+        });
+        
+        await chat.save();
+    
+        // 채팅 메시지 전송
+        io.to(roomId).emit('message', {
+          _id: chat._id,
+          __v: chat.__v,
+          chat: chat.chat,
+          createdAt: chat.createdAt,
+          updatedAt: chat.updatedAt,
+          user: {
+            id: user.id,
+            name: user.nickname,
+            profileImg: user.profileImage ? user.profileImage : null,
+          },
+        });
       } catch (error) {
-        cb({ok: false ,error: error.message })
+        console.error(error);
       }
     });
     
-    socket.on("sendMessage", async (message, cb) => {
-      try {
-        // 소켓아이디로 유저 찾기
-        const user = await User.findOne({socketId: socket.id});
-        // 메세지 저장(찾은 유저 넣기)
-        const newMessage = await chatController.saveChat(message,user);
-        // io서버에 접속한 모두에게 알려줘야됨 
-        io.emit("message", newMessage);
-        cb({ ok: true })
-      } catch (error) {
-        cb({ ok: false, error: error.message })
-      }
-    })
-   })
+  
+    // 연결이 끊길 때
+    socket.on('disconnect', async () => {
+      console.log('User disconnected');
+  
+      // 사용자의 소켓 ID를 null로 업데이트
+      await User.findOneAndUpdate({ socketId: socket.id }, { socketId: null });
+    });
+  });
 }

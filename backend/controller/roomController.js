47a0e0e49b8253a947b3c184f2d8d6c93d9a79f6
cyj <cyd5538@naver.com>
2024-01-24 22:26:44 +0900

    Feat: room message get api

--- backend/controller/roomController.js
+++ backend/controller/roomController.js
@@ -47,8 +47,46 @@ const deleteRoom = asyncHandler(async (req, res) => {
   }
 });
 
+const getRoomMessages = asyncHandler(async (req, res) => {
+  const roomId = req.params.id;
+
+  try {
+    const room = await Room.findById(roomId).populate({
+      path: 'chats',
+      options: {
+        sort: { createdAt: 'desc' },
+      },
+      populate: {
+        path: 'user',
+        select: '_id nickname profileImage',
+      },
+    });
+
+    if (!room) {
+      res.status(404).json({ message: '채팅방을 찾을 수 없습니다.' });
+      return;
+    }
+
+    const messages = room.chats.map(chat => ({
+      __v: chat.__v,
+      _id: chat._id,
+      user: chat.user,
+      chat: chat.chat,
+      createdAt: chat.createdAt,
+      updatedAt: chat.updatedAt,
+    }));
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: '서버 오류' });
+  }
+});
+
+
 module.exports = {
   createRoom,
   getRooms,
-  deleteRoom
+  deleteRoom,
+  getRoomMessages
 }
--- backend/routes/roomRoute.js
+++ backend/routes/roomRoute.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {
   createRoom,
   getRooms,
-  deleteRoom
+  deleteRoom,
+  getRoomMessages  
 } = require('../controller/roomController');
 const { protect } = require('../middlewear/authmiddlewear');
 
 router.route('/').post(protect, createRoom).get(getRooms);
-router.route('/:id').delete(protect, deleteRoom)
+router.route('/:id').delete(protect, deleteRoom);
+router.route('/:id/messages').get(protect, getRoomMessages);
 
 module.exports = router;

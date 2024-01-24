40f0b35de6091ae34f843100b47457de4abf2216
cyj <cyd5538@naver.com>
2024-01-24 13:11:21 +0900

    Feat: chat update
    채팅메세지 시간 넣고 내챗은 이름과 아바타 hidden

--- client/src/components/Chat/ChatContainer.tsx
+++ client/src/components/Chat/ChatContainer.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { ChatType } from "@/types/types";
+import { format, parseISO } from "date-fns";
 
 interface ChatContainerProps {
   messageList: ChatType[];
@@ -24,26 +25,36 @@ const ChatContainer: React.FC<ChatContainerProps> = ({ messageList, user }) => {
         <div
           key={index}
           className={`mb-2 gap-[10px] px-2 ${
-            message.user.name === user ? 'flex justify-end' : 'flex items-start justify-end flex-row-reverse'
+            message.user.name === user ? 'flex justify-end items-start ' : 'flex items-start justify-end flex-row-reverse'
           }`}
         >
-          <div
-            className={`rounded p-2 ${
+          <div className={`mb-2 gap-[10px] px-2 ${
+            message.user.name !== user ? 'flex justify-end items-end ' : 'flex items-end justify-end flex-row-reverse'
+          }`}>
+            <div className={`rounded p-2  ${
               message.user.name === user
                 ? 'bg-green-800 text-white'
                 : 'bg-white text-black'
+            }`}>
+              {message.chat}
+            </div>
+            <span className="text-xs">
+              {format(parseISO(message.createdAt), 'hh시mm분')}
+            </span>
+          </div>
+          <div
+            className={`${
+              message.user.name === user && 'hidden'
             }`}
           >
-            {message.chat}
-          </div>
-          <div>
             <img 
               className="rounded-full"
               src={message.user.profileImg ? message.user.profileImg : "/public/user.png"} 
               alt={message.user.name}
-              width={50}
-              height={50}
+              width={40}
+              height={40}
             />
+            <span>{message.user.name}</span>
           </div>
         </div>
       ))}

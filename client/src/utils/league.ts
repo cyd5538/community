269bb3e5dec9b406cbeaa1078f47e259b8b6f3bd
cyj <cyd5538@naver.com>
2024-01-27 19:10:15 +0900

    Design: league메뉴 파일

---
+++ client/src/utils/league.ts
@@ -0,0 +1,22 @@
+export const leagueData = [
+  {
+    "name": "EPL",
+    "league": "PL",
+  },
+  {
+    "name": "LaLiga",
+    "league": "PD",
+  },
+  {
+    "name": "Bundes",
+    "league": "BL1",
+  },
+  {
+    "name": "SerieA",
+    "league": "SA",
+  },
+  {
+    "name": "Ligue 1",
+    "league": "FL1",
+  }
+];

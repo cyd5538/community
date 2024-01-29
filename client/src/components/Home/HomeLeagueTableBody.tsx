22deee75862629b7cae307af02c389275511932e
cyj <cyd5538@naver.com>
2024-01-29 14:38:01 +0900

    Feat: league rank tooltip
    표에 리그에 마우스올렸을때 팀 이름툴팁으로

--- client/src/components/Home/HomeLeagueTableBody.tsx
+++ client/src/components/Home/HomeLeagueTableBody.tsx
@@ -3,23 +3,47 @@ import {
   TableCell,
   TableRow,
 } from "@/components/ui/table"
+import { useState } from "react";
 
 interface HomeLeagueTableBodyProp {
   rankData: LeagueDatatable
 }
 
 const HomeLeagueTableBody:React.FC<HomeLeagueTableBodyProp> = ({rankData}) => {
+  const [tooltip, setTooltip] = useState<string | null>(null);
+
+  const handleMouseOver = (teamName: string) => {
+    setTooltip(teamName);
+  };
+
+  const handleMouseOut = () => {
+    setTooltip(null);
+  };
+
   return (
-    <TableRow className="text-xs">
+    <TableRow 
+      onMouseOver={() => handleMouseOver(rankData.team.name)}       
+      onMouseOut={handleMouseOut}
+      className="text-xs relative cursor-pointer"
+    >
       <TableCell className="text-center">{rankData.position}</TableCell>
       <TableCell className="text-center">
-        <img src={rankData.team.crest} alt="" className='w-6 h-6'/>
+        <img 
+          src={rankData.team.crest} 
+          alt="" 
+          className='w-6 h-6'
+        />
       </TableCell>
       <TableCell className="text-center">{rankData.playedGames}</TableCell>
       <TableCell className="text-center">{rankData.points}</TableCell>
       <TableCell className="text-center">{rankData.won}</TableCell>
       <TableCell className="text-center">{rankData.draw}</TableCell>
       <TableCell className="text-center">{rankData.lost}</TableCell>
+      {tooltip && (
+        <div className="absolute top-[-30px] left-24 bg-green-400 w-20 text-center text-white p-2 rounded-md">
+          {tooltip}
+        </div>
+      )}
     </TableRow>
   )
 }

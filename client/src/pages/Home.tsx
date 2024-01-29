1d4f2a494559ac455a0ee1988d750937b903ce76
cyj <cyd5538@naver.com>
2024-01-29 16:51:57 +0900

    Design: home design width값 변경

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -10,13 +10,13 @@ const Home = () => {
     <div className='w-full flex flex-col relative'>
       <Searchbar />
       <PostModal />
-      <div className='pt-16 w-full flex gap-4'>
-        <div className='w-full md:w-2/3 border-gray-10 border-[1px] p-4'>
+      <div className='pt-16 w-full flex gap-8'>
+        <div className='w-full md:w-3/5 border-gray-10 border-[1px] p-4'>
           <Posts />
         </div>
         <div className='hidden md:w-1/3 md:flex flex-col gap-4'>
-          <div className=''>
-            <RoomList cardHeight={200} cardWidth={390}/>
+          <div>
+            <RoomList cardHeight={500} cardWidth={390}/>
           </div>
           <div className='w-[390px]'>
             <HomeLeagueTable />

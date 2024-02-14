8c437f1e75cac7858966e99a48b43bdb5f71cf59
cyj <cyd5538@naver.com>
2024-02-14 21:48:57 +0900

    Feat: query parameters handling

--- client/src/pages/Football.tsx
+++ client/src/pages/Football.tsx
@@ -4,47 +4,71 @@ import LeagueScoreRank from '@/components/Football/LeagueScoreRank'
 import LeagueTeamPlayerSelect from '@/components/Football/LeagueTeamPlayerSelect'
 import LeagueYearSelect from '@/components/Football/LeagueYearSelect'
 import { leagueData } from '@/utils/league'
-import { useLayoutEffect, useState } from 'react'
-import { useSearchParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 const Football = () => {
-  const [leagueChoice, setLeagueChoice] = useState<string>(leagueData[0].league)
-  const [teamPlayerSelect, setTeamPlaterSelect] = useState<string>("팀 순위")
-  const [season, setSeason] = useState<string>("2023");
   const [searchParams] = useSearchParams();
   const param = searchParams.get('league');
 
-  useLayoutEffect(() => {
-    setLeagueChoice(param ? param : "PL")
-    window.scrollTo(0, 0);
-  }, [param]);
+  const [leagueChoice, setLeagueChoice] = useState<string>(() => {
+    return param ? param : leagueData[0].league;
+  });
+  const [season, setSeason] = useState<string>("2023");
+  const [teamPlayerSelect, setTeamPlaterSelect] = useState<string>("팀 순위")
 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // 기본값 라우팅
+    if(!param) {
+      navigate(`?league=PL&season=2023`)
+    }
+
+    const leagueParam = param?.replace(/\?.*/, "");
+    const seasonMatch = param?.match(/season=(\d+)/);
+    const seasonParam = seasonMatch ? seasonMatch[1] : null;
+
+    setLeagueChoice(leagueParam ? leagueParam : "PL")
+    setSeason(seasonParam ? seasonParam : "2023")
+
+    window.scrollTo(0, 0);
+  }, [param, leagueChoice, season, navigate]);
+  
+  const handleLeagueChange = (e: string) => {
+    setLeagueChoice(e); 
+    navigate(`?league=${e}&season=${season}`)
+  };
+  
   const handleYearChange = (e: string) => {
     setSeason(e); 
+    navigate(`?league=${leagueChoice}&season=${e}`)
   };
 
+
   return (
-    <div className='pl-6 pt-6 pr-4 flex flex-col gap-8'>
+    <div className='pl-6 pt-6 pr-4 flex flex-col gap-4'>
       <LeagueTeamPlayerSelect 
         teamPlayerSelect={teamPlayerSelect}
         setTeamPlaterSelect={setTeamPlaterSelect}
       />
       <div className='flex gap-2'>
-        {leagueData?.map((league) =>
-          <LeagueRankSelect
-            key={league.name}
-            league={league}
-            leagueChoice={leagueChoice}
-            setLeagueChoice={setLeagueChoice}
-          />
-        )}
+        <LeagueRankSelect
+          leagueChoice={leagueChoice}
+          handleLeagueChange={handleLeagueChange}
+        />
+        <LeagueYearSelect 
+          season={season}
+          handleYearChange={handleYearChange}
+        />
       </div>
-      <LeagueYearSelect 
-        season={season}
-        handleYearChange={handleYearChange}
-      />
-      {teamPlayerSelect === "팀 순위" && <LeagueRank leagueChoice={leagueChoice} teamPlayerSelect={teamPlayerSelect}/>}
-      {teamPlayerSelect === "개인 순위" && <LeagueScoreRank 
+      {teamPlayerSelect === "팀 순위" && 
+      <LeagueRank 
+        leagueChoice={leagueChoice} 
+        teamPlayerSelect={teamPlayerSelect}/>
+      }
+      {teamPlayerSelect === "개인 순위" && 
+      <LeagueScoreRank 
         teamPlayerSelect={teamPlayerSelect}
         leagueChoice={leagueChoice}
         season={season}

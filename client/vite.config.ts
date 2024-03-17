998940fac9538d37efa0c5652ef567cfd75b07fc
cyj <cyd5538@naver.com>
2024-03-17 09:58:31 +0900

    Chore: vitest react-testing-libeary set up

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,19 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    pool: 'forks',
+    setupFiles: "./src/test/setup.ts",
+    css: true,
+  },
   server: {
     proxy: {
       '/api': {

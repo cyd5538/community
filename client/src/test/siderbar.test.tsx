9ec56b5186894fbedc3251101aba8bc3b91804f0
cyj <cyd5538@naver.com>
2024-03-27 09:04:20 +0900

    Test: sidebar test
    로그인 했을때 안했을때 사이드바 테스트

--- client/src/test/siderbar.test.tsx
+++ client/src/test/siderbar.test.tsx
@@ -2,6 +2,8 @@ import Sidebars from '@/components/sidebar/Sidebars';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
+
+
 describe("사이드바 테스트", () => {
 
   test('메뉴 아이콘이 7개보다 많은지', async () => {
@@ -37,5 +39,46 @@ describe("사이드바 테스트", () => {
 
     expect(sidebar).toHaveClass('w-[70px]');
     expect(roundForwardIcon).toBeInTheDocument();
+  });
+
+  test('로그인한 경우 메뉴 렌더링 테스트', async () => {
+    const mockUser = vi.fn();
+
+    vi.mock('useAuth', () => {
+      return {
+        user: mockUser(true),
+        logout: vi.fn(),
+      };
+    });
+
+    render(
+      <MemoryRouter>
+        <Sidebars />
+      </MemoryRouter>
+    );
+  
+    // user가 true면 login, sign up이 보이면 안됨
+    expect(screen.queryByRole('link', { name: 'Login' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('link', { name: 'Sign up' })).not.toBeInTheDocument();
+  });
+
+  test('로그인 안한 경우 메뉴 렌더링 테스트', async () => {
+    const mockUser = vi.fn();
+
+    vi.mock('useAuth', () => {
+      return {
+        user: mockUser(false),
+        logout: vi.fn(),
+      };
+    });
+
+    render(
+      <MemoryRouter>
+        <Sidebars />
+      </MemoryRouter>
+    );
+  
+    // user가 false면 Logout이 보이면 안됨
+    expect(screen.queryByRole('link', { name: 'Logout' })).not.toBeInTheDocument();
   });
 })
